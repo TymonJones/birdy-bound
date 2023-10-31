71f126772769a3ef71cb077b45c0169e6d883a82
TymonJones <101831045+Tjonte03@users.noreply.github.com>
2023-10-31 10:54:04 -0700

    added routes

--- server/app.py
+++ server/app.py
@@ -1,11 +1,79 @@
-from flask import Flask
+from flask import Flask, request, jsonify, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 
+# Initialize Flask App
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'  # You should use a secure, random secret key.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///golfapp.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Initialize Extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# User Loader for Flask-Login
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Models
+class User(UserMixin, db.Model):
+    # ...existing fields...
+    # Add UserMixin for Flask-Login compatibility
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+# ... Other models...
+
+# Routes
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    # Check if username or email already exists
+    if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
+        return jsonify({'message': 'User already exists'}), 409
+
+    new_user = User(username=username, email=email)
+    new_user.set_password(password)
+    
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data.get('username')).first()
+
+    if user and user.check_password(data.get('password')):
+        login_user(user)
+        return jsonify({'message': 'Logged in successfully'}), 200
+    return jsonify({'message': 'Invalid username or password'}), 401
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logged out successfully'}), 200
+
+# ...Additional endpoints...
+
+# Start the Flask app
+if __name__ == '__main__':
+    app.run(debug=True)
 
