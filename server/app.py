bc769313a5a96179939c482644cb942d7e7c12c7
TymonJones <101831045+Tjonte03@users.noreply.github.com>
2023-10-31 10:09:21 -0700

    first db migration

--- server/app.py
+++ server/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///golfapp.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ server/models.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    skill_level = db.Column(db.String(50))
+    bookings = db.relationship('Booking', backref='player', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class GolfCourse(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    location = db.Column(db.String(300), nullable=False)
+    # Additional fields can include address, description, facilities, etc.
+    tee_times = db.relationship('Booking', backref='course', lazy='dynamic')
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    course_id = db.Column(db.Integer, db.ForeignKey('golf_course.id'))
+    tee_time = db.Column(db.DateTime, nullable=False)
+    # You might want to add additional fields, such as status of booking, number of players, etc.
